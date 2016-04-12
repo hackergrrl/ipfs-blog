75d23a3227e33d82a077631a78a21f5d55fe31c3
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-04-11 22:07:12 -0700

    cli.js

---
+++ cli.js
@@ -0,0 +1,14 @@
+var publish = require('./index')
+var fs = require('fs')
+
+var files = fs.readdirSync(process.cwd())
+files = files.filter(function isMarkdown (name) {
+  return name.endsWith('.markdown') || name.endsWith('.md')
+})
+
+if (!files.length) {
+  console.error('no .md or .markdown files in this directory')
+  process.exit(1)
+}
+
+publish(files)
